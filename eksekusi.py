1653e6b8b5bc3dedc1f8f2d6747d49254540e0f6
cckaa <92702411+cckaa@users.noreply.github.com>
2021-10-31 03:51:20 +0700

    Add files via upload

---
+++ eksekusi.py
@@ -0,0 +1,13 @@
+print("Program Luas & Keliling")
+'''
+ini hanya komentar
+ini juga hanya komentar
+'''
+r = int(input("Masukan jari-jari = "))
+
+phi = 3.14
+luas = phi*r*r
+kel = 2*phi*r
+
+print("Luas lingkaran adalah - ", luas)
+print("Keliling lingkaran = ", kel)
